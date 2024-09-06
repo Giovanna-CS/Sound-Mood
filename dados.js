c4de7a9b35c1a2f76f8d3da19648acba79542cb1
Giovanna-CS <giovannacristine72@gmail.com>
2024-09-05 22:15:43 -0300

    First Commit

---
+++ dados.js
@@ -0,0 +1,32 @@
+let dados = [
+    {
+        titulo: "Felicidade",
+        descrição: "lorem ipsulum ablablablabla",
+        link : "https://open.spotify.com/playlist/5ukdwayJCYQKfCpajC5RQd?si=cdfadd3c638b4f5a"
+    },
+
+
+    {
+        titulo: "Tristeza",
+        descrição: "lorem ipsulum ablablablabla",
+        link : "https://open.spotify.com/playlist/5ukdwayJCYQKfCpajC5RQd?si=cdfadd3c638b4f5"
+    },
+
+    {
+        titulo: "tedio",
+        descrição: "lorem ipsulum ablablablabla",
+        link : "https://open.spotify.com/playlist/5ukdwayJCYQKfCpajC5RQd?si=cdfadd3c638b4f5"
+    },
+
+    {
+        titulo: "barganha",
+        descrição: "lorem ipsulum ablablablabla",
+        link : "https://open.spotify.com/playlist/5ukdwayJCYQKfCpajC5RQd?si=cdfadd3c638b4f5"
+    },
+
+    {
+        titulo: "euforia",
+        descrição: "lorem ipsulum ablablablabla",
+        link : "https://open.spotify.com/playlist/5ukdwayJCYQKfCpajC5RQd?si=cdfadd3c638b4f5"
+    },
+]
