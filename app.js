546088b72620bb2dca30b0336c8845904c6203e5
Giovanna-CS <giovannacristine72@gmail.com>
2024-09-07 23:26:50 -0300

    Primeiro Commit

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
-console.log(dados);
+function pesquisar() {
+    let section = document.getElementById("resultado-pesquisa"); //Obtém o elemento da seção onde os resultados da pesquisa serão exibidos. 
 
-let section = document.getElementById("resultado-pesquisa")
-console.log(section);
+    let campoPesquisa = document.getElementById ("campo-pesquisa").value; 
+    // Obtém o valor do campo de pesquisa.
 
-let resultados = ""
 
-for (let dado of dados) {
-    resultados += `
-    <div class="item-resultado">
-         <h2>${dado.titulo}</h2>
-         <p class="descrição-resultado">${dado.descrição}ulalau vc ta feliz veja essa playlist <a href="${dado.link}"><img src="SpongeBob_SquarePants_personagem.png" alt=""></a></p>
-    </div> `
+    if (campoPesquisa == "") {
+        section.innerHTML = "<p> Nada foi encontrado </p>"
+        return
+    }
+
+     // Verifica se o campo de pesquisa está vazio. Se estiver, exibe uma mensagem indicando que nada foi encontrado e encerra a função.
+    campoPesquisa = campoPesquisa.toLowerCase();  // Converte o texto do campo de pesquisa para minúsculas para facilitar a comparação.
+
+    let resultados = "";
+    let titulo = "";
+    let descrição = "";
+    let tags = "";
+
+for (let dado of dados) { // Percorre todos os dados disponíveis.
+    titulo =dado.titulo.toLowerCase ()
+    descrição =dado.descrição.toLowerCase ()
+    tags =dado.tags.toLowerCase ()
+
+    if (titulo.includes(campoPesquisa) || (descrição.includes(campoPesquisa)) || tags.includes(campoPesquisa)){ 
+        resultados += // Se houver correspondência, adiciona o item aos resultados.
+       `<div class="item-resultado">
+           <h2>${dado.titulo}</h2>
+           <p class="descrição-resultado">${dado.descrição} <a href="${dado.link}"><img src="midias/spotifyimg.jpg" </a></p>
+       </div> `;
+   }
 }
 
-section.innerHTML = resultados
+// Exibe os resulados na seção de pesquisa.
+section.innerHTML = resultados;    
+
+}
 
--- dados.js
+++ dados.js
@@ -1,20 +1,23 @@
 let dados = [
     {
-        titulo: "Felicidade",
-        descrição: "lorem ipsulum ablablablabla",
-        link : "https://open.spotify.com/playlist/5ukdwayJCYQKfCpajC5RQd?si=cdfadd3c638b4f5a"
+        titulo: "⭐",
+        descrição: "Vimos que você está radiante e queremos te ajudar a manter esse astral lá em cima! ☀️ Essa playlist foi feita especialmente para você, com as músicas mais alegres e dançantes que encontramos. Prepare-se para uma festa particular! ",
+        link : "https://open.spotify.com/playlist/5ukdwayJCYQKfCpajC5RQd?si=cdfadd3c638b4f5a",
+        tags : "feliz alegria felicidade gratidão amor esperança paz"
     },
 
     {
-        titulo: "Tristeza",
-        descrição: "lorem ipsulum ablablablabla",
-        link : "https://open.spotify.com/playlist/5ukdwayJCYQKfCpajC5RQd?si=cdfadd3c638b4f5"
+        titulo: "⭐",
+        descrição: "Olá, vimos que você está se sentindo um pouco para baixo e queremos te ajudar a levantar esse astral! ☀️ Essa playlist foi cuidadosamente selecionada para te trazer conforto e te inspirar.  Prepare-se para uma jornada musical que vai te fazer sorrir e dançar!",
+        link : "https://open.spotify.com/playlist/491Ui9O61DAuddpXlTbGFO?si=5293345c6e684744",
+        tags :"triste barganha tristeza melancolia melancolica melancolico angustia"
     },
 
     {
-        titulo: "tedio",
-        descrição: "lorem ipsulum ablablablabla",
-        link : "https://open.spotify.com/playlist/5ukdwayJCYQKfCpajC5RQd?si=cdfadd3c638b4f5"
+        titulo: "⭐",
+        descrição: "Que tal dar um basta no tédio? ⚡ Essa playlist te tirará da rotina e te levará para um mundo de novas aventuras. Prepare-se para descobrir músicas que vão te surpreender e te energizar!",
+        link : "https://open.spotify.com/playlist/5ukdwayJCYQKfCpajC5RQd?si=cdfadd3c638b4f5",
+        tags : "tedio indiferença desanimo"
     },
 
 ]
